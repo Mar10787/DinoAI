366e95261c4915d2bca104b3885b69422acb656e
Martino <martinobennguyen@gmail.com>
2025-02-22 16:41:57 +1000

    Commenting Structure

--- dinoAI.py
+++ dinoAI.py
@@ -1,1 +1,18 @@
 # Using RL to train AI to play dino on google chrome browser
+# chrome://dino/
+
+# 1. Installing and Importing Dependencies
+# !pip3 install torch torchvision torchaudio --index-url https://download.pytorch.org/whl/cu126
+# !pip install stable-baselines3[extra] protobuf==3.20.*
+# !pip install mss pydirectinput pytesseract
+
+
+# 2.  Build the Environment
+# 2.1 Create Environment
+# 2.2 Test the Environment
+
+# 3. Train Model
+# 3.1 Create Callback
+# 3.2 Build DQN Model and Train
+
+# 4. Test out Model
